a58fec3428223f767091929e1379e56de6afe759
sakshigund24 <sakshi.gund@somaiya.edu>
2026-06-14 01:33:29 +0530

    Temporary cors test

--- backend/main.py
+++ backend/main.py
@@ -59,12 +59,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:3000",
-        settings.FRONTEND_URL,
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
